a3d0838130ba8302f3cb3c0361c4d9e1298114a5
E-Viskhan <viskhan11@gmail.com>
2021-10-06 02:48:50 +0300

    create: close modal on click outside modal and esc

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// сделать поле email
 $(document).ready(function() {
+  // Начало кода яндекс карт на сайте
   // Функция ymaps.ready() будет вызвана, когда
   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
   ymaps.ready(init);
@@ -16,7 +16,9 @@ $(document).ready(function() {
           zoom: 17
       });
   }
+  // Конец кода яндекс карт
 
+  // Код слайдера в секции отель
   var hotelSwiper = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
@@ -30,7 +32,9 @@ $(document).ready(function() {
       prevEl: '.hotel-slider__button--prev',
     },
   });
-
+  // Конец кода слайдера в секции отель 
+  
+  // Код для слайдера в секции отзывов
   var reviewsSlider = new Swiper('.reviews-slider', {
     // Optional parameters
     loop: true,
@@ -44,29 +48,49 @@ $(document).ready(function() {
       prevEl: '.reviews-slider__button--prev',
     },
   });
+  // Конец кода слайдера в секции отзывов
 
-
+  // Код для эффекта параллакса в секции newsletter
   $('.newsletter-parallax').parallax({imageSrc: 'img/newsletter-bg.jpg', speed: 0.7});
+  // Конец кода эффекта параллакса в секции newsletter
 
+  // Код для главного меню на мобильной версии
   var navbarButton = $('.menu-button');
   navbarButton.on('click', function(){
     $('.navbar-bottom').toggleClass('navbar-bottom--visible');
   });
-  
+  // Конец кода главного меню
+
+  // Код для модального окна
   var modalOpenButton = $('[data-toggle=modal]');
   var modalCloseButton = $('.modal__close');
   var modalOverlay = $('.modal__overlay');
   var modalDialog = $('.modal__dialog');
   modalOpenButton.on('click', openModal);
-  modalCloseButton.on('click', closeModal)
+  modalCloseButton.on('click', closeModal);
+
   function openModal(){
     modalOverlay.addClass('modal__overlay--visible')
     modalDialog.addClass('modal__dialog--visible')
+    $(document).on('keydown', function(e){
+      if (e.which === 27) closeModal()
+    });
   }
 
   function closeModal(event){
-    event.preventDefault()
+    // Проверка на сущестовавание event, потому что вызов данного метода из метода open,
+    // когда мы нажимаем esc не передает нам event
+    if (event !== undefined) event.preventDefault()
     modalOverlay.removeClass('modal__overlay--visible')
     modalDialog.removeClass('modal__dialog--visible')
   }
+
+  // Закрытие модального окна при клике вне области модального окна
+  $(document).mouseup(function (event){ // событие клика по веб-документу
+		if (!modalDialog.is(event.target) // если клик был не по нашему блоку
+      && modalDialog.has(event.target).length === 0) { // и не по его дочерним элементам
+      closeModal(); // скрываем его
+    }
+  });
+  // Конец кода модального окна
 });
