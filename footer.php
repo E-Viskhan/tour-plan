62556b2ba61d3803be772d743285af5815c847df
E-Viskhan <viskhan11@gmail.com>
2021-10-09 03:06:11 +0300

    fix: added local jQuery in project

--- footer.php
+++ footer.php
@@ -271,7 +271,7 @@
     </div>
     <!-- /.modal -->
     <script src="js/swiper-bundle.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/jquery.validate.min.js"></script>
     <script src="js/parallax.min.js"></script>
     <script src="js/jquery.mask.min.js"></script>
